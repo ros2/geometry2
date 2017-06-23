42d462de5f4bb69f4b086ef056496c97379dedff
Tully Foote <tfoote@osrfoundation.org>
2017-06-23 13:53:28 -0700

    initialize whole quaternion

--- tf2_geometry_msgs/test/test_tf2_geometry_msgs.cpp
+++ tf2_geometry_msgs/test/test_tf2_geometry_msgs.cpp
@@ -137,6 +137,9 @@ int main(int argc, char **argv){
   t.transform.translation.y = 20;
   t.transform.translation.z = 30;
   t.transform.rotation.x = 1;
+  t.transform.rotation.y = 0;
+  t.transform.rotation.z = 0;
+  t.transform.rotation.w = 0;
   t.header.stamp = tf2_ros::toMsg(tf2::timeFromSec(2));
   t.header.frame_id = "A";
   t.child_frame_id = "B";
