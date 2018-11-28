bc884c11915f471b51b494984c340df768e21d37
Ingo Luetkebohle (CR/AEX3) <ingo.luetkebohle@de.bosch.com>
2018-11-28 10:50:46 +0000

    Factor out second test

--- tf2_filter/test/test_filter.cpp
+++ tf2_filter/test/test_filter.cpp
@@ -49,24 +49,18 @@ TEST_F(TF2FilterTest, filter_empty)
   TF2Filter filter(map);
 
   ASSERT_FALSE(filter.filter(msg));
-
-  TargetSet ts;
-  ts.emplace("arm1");
-  map["base_link"] = ts;
-
-  filter = TF2Filter(map);
-  ASSERT_TRUE(filter.filter(msg));
-
-  // basic test
-  // ASSERT_FALSE(buffer.canTransform("foo", "bar", builtin_interfaces::msg::Time(101, 0)));
-
-  // verify it's been set
-  // ASSERT_TRUE(buffer.canTransform("foo", "bar", builtin_interfaces::msg::Time(101, 0)));
-
-  // verify the data's been cleared
-  // ASSERT_FALSE(buffer.canTransform("foo", "bar", builtin_interfaces::msg::Time(101, 0)));
 }
 
+TEST_F(TF2FilterTest, filter_single)
+{
+  TargetSet ts;
+  ts.emplace("arm1");
+  FilterMap map;
+  map["base_link"] = ts;
+
+  TF2Filter filter = TF2Filter(map);
+  ASSERT_TRUE(filter.filter(msg));
+}
 
 int main(int argc, char ** argv)
 {
