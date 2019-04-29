edfbb989fc014d5a988f76303a4a6c5e31615bd2
Emerson Knapp <eknapp@amazon.com>
2019-04-29 16:57:32 -0700

    Fix call to create_publisher after API changed
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>

--- tf2_ros/include/tf2_ros/static_transform_broadcaster.h
+++ tf2_ros/include/tf2_ros/static_transform_broadcaster.h
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) 2008, Willow Garage, Inc.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
  *     * Neither the name of the Willow Garage, Inc. nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -44,8 +44,8 @@ namespace tf2_ros
 {
 
 
-/** \brief This class provides an easy way to publish coordinate frame transform information.  
- * It will handle all the messaging and stuffing of messages.  And the function prototypes lay out all the 
+/** \brief This class provides an easy way to publish coordinate frame transform information.
+ * It will handle all the messaging and stuffing of messages.  And the function prototypes lay out all the
  * necessary data needed for each message.  */
 
 class StaticTransformBroadcaster{
@@ -64,9 +64,10 @@ public:
 
     using MessageT = tf2_msgs::msg::TFMessage;
     using PublisherT = ::rclcpp::Publisher<MessageT, AllocatorT>;
+    rclcpp::PublisherEventCallbacks callbacks;
     publisher_ = rclcpp::create_publisher<MessageT, AllocatorT, PublisherT>(
       node_topics_interface.get(), "/tf_static",
-      custom_qos_profile, false, std::make_shared<AllocatorT>());
+      custom_qos_profile, callbacks, nullptr, false, std::make_shared<AllocatorT>());
   }
 
   /** \brief Send a TransformStamped message
