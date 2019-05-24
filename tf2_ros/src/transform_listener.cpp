ff8be6a13051f6759af8163acd10b10d87d28a46
bpwilcox <bpwilcox@eng.ucsd.edu>
2019-05-24 00:17:58 -0700

    stop spinning TransformListener thread node in destructor (#114)
    * stop spinning TransformListener thread node in destructor
    
    Signed-off-by: bpwilcox <bpwilcox@eng.ucsd.edu>

--- tf2_ros/src/transform_listener.cpp
+++ tf2_ros/src/transform_listener.cpp
@@ -54,17 +54,19 @@ TransformListener::TransformListener(tf2::BufferCore & buffer, bool spin_thread)
 
 TransformListener::~TransformListener()
 {
+  stop_thread_ = true;
 }
 
 void TransformListener::initThread(
   rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base_interface)
 {
+  stop_thread_ = false;
   // This lambda is required because `std::thread` cannot infer the correct
   // rclcpp::spin, since there are more than one versions of it (overloaded).
   // see: http://stackoverflow.com/a/27389714/671658
   // I (wjwwood) chose to use the lamda rather than the static cast solution.
-  auto run_func = [](rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base_interface) {
-      return rclcpp::spin(node_base_interface);
+  auto run_func = [&](rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base_interface) {
+      while (!stop_thread_ && rclcpp::ok()) {rclcpp::spin_some(node_base_interface);}
     };
   dedicated_listener_thread_ = thread_ptr(
     new std::thread(run_func, node_base_interface),
