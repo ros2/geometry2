c0a7156f81c6fee26c32feaa6d4c45a6f7bf6e42
Shane Loretz <sloretz@osrfoundation.org>
2019-09-13 14:56:18 -0400

    Remove unnecessary str()
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- tf2_ros/src/tf2_ros/transform_listener.py
+++ tf2_ros/src/tf2_ros/transform_listener.py
@@ -100,11 +100,11 @@ class TransformListener:
         self.node.destroy_subscription(self.tf_static_sub)
 
     def callback(self, data):
-        who = str("default_authority")
+        who = 'default_authority'
         for transform in data.transforms:
             self.buffer.set_transform(transform, who)
 
     def static_callback(self, data):
-        who = str("default_authority")
+        who = 'default_authority'
         for transform in data.transforms:
             self.buffer.set_transform_static(transform, who)
