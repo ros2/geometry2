7e745c7eab1ae3dd43aa8aca426af534c5b6be09
Chris Lalancette <clalancette@openrobotics.org>
2019-09-17 14:47:19 +0000

    Remove unnecessary and blacklisted actionlib_msgs dependency.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- tf2_ros/setup.py
+++ tf2_ros/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['tf2_ros'],
     package_dir={'': 'src'},
-    requires=['rospy', 'actionlib', 'actionlib_msgs', 'tf2_msgs',
+    requires=['rospy', 'actionlib', 'tf2_msgs',
               'tf2_py', 'geometry_msgs']
 )
 
