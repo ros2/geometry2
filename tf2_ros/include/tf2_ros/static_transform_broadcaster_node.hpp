eee51d8221e4f2e777797ba2da22417fc036d540
Hunter L. Allen <hunterlallen@protonmail.com>
2019-10-18 00:20:28 -0400

    Create a static transform component for composition
    Signed-off-by: Hunter L. Allen <hunterlallen@protonmail.com>

---
+++ tf2_ros/include/tf2_ros/static_transform_broadcaster_node.hpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2019, Open Source Robotics Foundation, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef TF2_ROS__STATIC_TRANSFORM_BROADCASTER_NODE_HPP_
+#define TF2_ROS__STATIC_TRANSFORM_BROADCASTER_NODE_HPP_
+
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "tf2_ros/static_transform_broadcaster_visibility_control.h"
+
+namespace tf2_ros
+{
+class StaticTransformBroadcasterNode final : public rclcpp::Node
+{
+public:
+  STATIC_TRANSFORM_BROADCASTER_PUBLIC
+  explicit StaticTransformBroadcasterNode(const rclcpp::NodeOptions & options);
+
+  STATIC_TRANSFORM_BROADCASTER_PUBLIC
+  ~StaticTransformBroadcasterNode() override = default;
+
+private:
+  std::unique_ptr<tf2_ros::StaticTransformBroadcaster> broadcaster_;
+};
+}
+#endif  // TF2_ROS__STATIC_TRANSFORM_BROADCASTER_NODE_HPP_
