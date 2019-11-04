8110a3c837f492145b60dd1f6e6f6aa4ceb8a554
ahcorde <ahcorde@gmail.com>
2019-11-04 11:04:17 +0100

    Adding tf2_tools support for view_frames

--- tf2_tools/scripts/view_frames.py
+++ tf2_tools/scripts/view_frames.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # Copyright (c) 2008, Willow Garage, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
 #     * Neither the name of the Willow Garage, Inc. nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,43 +28,67 @@
 
 # author: Wim Meeussen
 
-import rospy
+import rclpy
 import tf2_py as tf2
 import yaml
 import subprocess
 from tf2_msgs.srv import FrameGraph
 import tf2_ros
+import time
 
-def main():
-    rospy.init_node('view_frames')
-    
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = rclpy.create_node('view_frames')
+
+    buffer = tf2_ros.Buffer(debug=True, node=node)
+    listener = tf2_ros.TransformListener(buffer, node, spin_thread=False)
+
+    executor = rclpy.executors.SingleThreadedExecutor()
+    executor.add_node(node)
+
     # listen to tf for 5 seconds
-    rospy.loginfo('Listening to tf data during 5 seconds...')
-    rospy.sleep(0.00001)
-    buffer = tf2_ros.Buffer()
-    listener = tf2_ros.TransformListener(buffer)
-    rospy.sleep(5.0)
+    node.get_logger().info('Listening to tf data during 5 seconds...')
+    start_time = time.time()
+    while (time.time() - start_time) < 5.0:
+        rclpy.spin_once(node, timeout_sec=0.1)
 
-    rospy.loginfo('Generating graph in frames.pdf file...')
-    rospy.wait_for_service('~tf2_frames')
-    srv = rospy.ServiceProxy('~tf2_frames', FrameGraph)
-    data = yaml.load(srv().frame_yaml)
-    with open('frames.gv', 'w') as f:
-        f.write(generate_dot(data))
-    subprocess.Popen('dot -Tpdf frames.gv -o frames.pdf'.split(' ')).communicate()
+    node.get_logger().info('Generating graph in frames.pdf file...')
 
-def generate_dot(data):
+    cli = node.create_client(FrameGraph, 'tf2_frames')
+    req = FrameGraph.Request()
+    while not cli.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('service not available, waiting again...')
+
+    future = cli.call_async(req)
+    rclpy.spin_until_future_complete(node, future)
+
+    try:
+        result = future.result()
+    except Exception as e:
+        node.get_logger().info('Service call failed %r' % (e,))
+    else:
+        node.get_logger().info(
+            'Result:'+ str(result) )
+        data = yaml.load(result.frame_yaml)
+        with open('frames.gv', 'w') as f:
+           f.write(generate_dot(data, node))
+        subprocess.Popen('dot -Tpdf frames.gv -o frames.pdf'.split(' ')).communicate()
+    node.destroy_node()
+    rclpy.shutdown()
+
+def generate_dot(data, node):
     if len(data) == 0:
         return 'digraph G { "No tf data received" }'
 
     dot = 'digraph G {\n'
-    for el in data: 
+    for el in data:
         map = data[el]
         dot += '"'+map['parent']+'" -> "'+str(el)+'"'
         dot += '[label=" '
         dot += 'Broadcaster: '+map['broadcaster']+'\\n'
         dot += 'Average rate: '+str(map['rate'])+'\\n'
-        dot += 'Buffer length: '+str(map['buffer_length'])+'\\n' 
+        dot += 'Buffer length: '+str(map['buffer_length'])+'\\n'
         dot += 'Most recent transform: '+str(map['most_recent_transform'])+'\\n'
         dot += 'Oldest transform: '+str(map['oldest_transform'])+'\\n'
         dot += '"];\n'
@@ -72,10 +96,9 @@ def generate_dot(data):
             root = map['parent']
     dot += 'edge [style=invis];\n'
     dot += ' subgraph cluster_legend { style=bold; color=black; label ="view_frames Result";\n'
-    dot += '"Recorded at time: '+str(rospy.Time.now().to_sec())+'"[ shape=plaintext ] ;\n'
+    dot += '"Recorded at time: '+str(node.get_clock().now())+'"[ shape=plaintext ] ;\n'
     dot += '}->"'+root+'";\n}'
     return dot
-
 
 if __name__ == '__main__':
     main()
