6a8dc8eace0f3d426cf1a03c71eb8d5ff7545cf5
ahcorde <ahcorde@gmail.com>
2019-12-17 09:56:23 +0100

    test_tf2 ported test_statis_publisher

--- test_tf2/test/test_static_publisher.cpp
+++ test_tf2/test/test_static_publisher.cpp
@@ -31,72 +31,93 @@
 #include <tf2/buffer_core.h>
 #include "tf2/exceptions.h"
 #include <tf2_ros/static_transform_broadcaster.h>
-#include <ros/ros.h>
-#include "rostest/permuter.h"
+#include <rclcpp/rclcpp.hpp>
+#include "permuter.hpp"
 
 #include "tf2_ros/transform_listener.h"
 
 TEST(StaticTransformPublisher, a_b_different_times)
 {
-  tf2_ros::Buffer mB;
-  tf2_ros::TransformListener tfl(mB);
-  EXPECT_TRUE(mB.canTransform("a", "b", builtin_interfaces::msg::Time(), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("a", "b", builtin_interfaces::msg::Time(100), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("a", "b", builtin_interfaces::msg::Time(1000), tf2::durationFromSec(1.0)));
+  auto node = rclcpp::Node::make_shared("StaticTransformPublisher_a_b_different_times_test");
+
+  rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
+  tf2_ros::Buffer mB(clock);
+  tf2_ros::TransformListener tfl(mB, node, false);
+
+  EXPECT_TRUE(mB.canTransform("a", "b", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "b", tf2_ros::toMsg(tf2::timeFromSec(100)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "b", tf2_ros::toMsg(tf2::timeFromSec(1000)), tf2::durationFromSec(1.0)));
+
+  rclcpp::shutdown();
 };
 
 TEST(StaticTransformPublisher, a_c_different_times)
 {
-  tf2_ros::Buffer mB;
-  tf2_ros::TransformListener tfl(mB);
-  EXPECT_TRUE(mB.canTransform("a", "c", builtin_interfaces::msg::Time(), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("a", "c", builtin_interfaces::msg::Time(100), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("a", "c", builtin_interfaces::msg::Time(1000), tf2::durationFromSec(1.0)));
+  auto node = rclcpp::Node::make_shared("StaticTransformPublisher_a_c_different_times_test");
+
+  rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
+  tf2_ros::Buffer mB(clock);
+  tf2_ros::TransformListener tfl(mB, node, false);
+
+  EXPECT_TRUE(mB.canTransform("a", "c", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "c", tf2_ros::toMsg(tf2::timeFromSec(100)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "c", tf2_ros::toMsg(tf2::timeFromSec(1000)), tf2::durationFromSec(1.0)));
+
+  rclcpp::shutdown();
 };
 
 TEST(StaticTransformPublisher, a_d_different_times)
 {
-  tf2_ros::Buffer mB;
-  tf2_ros::TransformListener tfl(mB);
-  geometry_msgs::TransformStamped ts;
+  auto node = rclcpp::Node::make_shared("StaticTransformPublisher_a_d_different_times_test");
+
+  rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
+  tf2_ros::Buffer mB(clock);
+  tf2_ros::TransformListener tfl(mB, node, false);
+
+  geometry_msgs::msg::TransformStamped ts;
   ts.transform.rotation.w = 1;
   ts.header.frame_id = "c";
-  ts.header.stamp = builtin_interfaces::msg::Time(10.0);
+  ts.header.stamp = tf2_ros::toMsg(tf2::timeFromSec(10));
   ts.child_frame_id = "d";
 
   // make sure listener has populated
-  EXPECT_TRUE(mB.canTransform("a", "c", builtin_interfaces::msg::Time(), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("a", "c", builtin_interfaces::msg::Time(100), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("a", "c", builtin_interfaces::msg::Time(1000), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "c", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "c", tf2_ros::toMsg(tf2::timeFromSec(100)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "c", tf2_ros::toMsg(tf2::timeFromSec(1000)), tf2::durationFromSec(1.0)));
 
   mB.setTransform(ts, "authority");
   //printf("%s\n", mB.allFramesAsString().c_str());
-  EXPECT_TRUE(mB.canTransform("c", "d", builtin_interfaces::msg::Time(10), tf2::durationFromSec(0)));
+  EXPECT_TRUE(mB.canTransform("c", "d", tf2_ros::toMsg(tf2::timeFromSec(10)), tf2::durationFromSec(0)));
 
-  EXPECT_TRUE(mB.canTransform("a", "d", builtin_interfaces::msg::Time(), tf2::durationFromSec(0)));
-  EXPECT_FALSE(mB.canTransform("a", "d", builtin_interfaces::msg::Time(1), tf2::durationFromSec(0)));
-  EXPECT_TRUE(mB.canTransform("a", "d", builtin_interfaces::msg::Time(10), tf2::durationFromSec(0)));
-  EXPECT_FALSE(mB.canTransform("a", "d", builtin_interfaces::msg::Time(100), tf2::durationFromSec(0)));
+  EXPECT_TRUE(mB.canTransform("a", "d", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(0)));
+  EXPECT_FALSE(mB.canTransform("a", "d", tf2_ros::toMsg(tf2::timeFromSec(1)), tf2::durationFromSec(0)));
+  EXPECT_TRUE(mB.canTransform("a", "d", tf2_ros::toMsg(tf2::timeFromSec(10)), tf2::durationFromSec(0)));
+  EXPECT_FALSE(mB.canTransform("a", "d", tf2_ros::toMsg(tf2::timeFromSec(100)), tf2::durationFromSec(0)));
 
+  rclcpp::shutdown();
 };
 
 TEST(StaticTransformPublisher, multiple_parent_test)
 {
-  tf2_ros::Buffer mB;
-  tf2_ros::TransformListener tfl(mB);
-  tf2_ros::StaticTransformBroadcaster stb;
-  geometry_msgs::TransformStamped ts;
+  auto node = rclcpp::Node::make_shared("StaticTransformPublisher_a_d_different_times_test");
+
+  rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
+  tf2_ros::Buffer mB(clock);
+  tf2_ros::TransformListener tfl(mB, node, false);
+
+  tf2_ros::StaticTransformBroadcaster stb(node);
+  geometry_msgs::msg::TransformStamped ts;
   ts.transform.rotation.w = 1;
   ts.header.frame_id = "c";
-  ts.header.stamp = builtin_interfaces::msg::Time(10.0);
+  ts.header.stamp = tf2_ros::toMsg(tf2::timeFromSec(10));
   ts.child_frame_id = "d";
 
   stb.sendTransform(ts);
 
   // make sure listener has populated
-  EXPECT_TRUE(mB.canTransform("a", "d", builtin_interfaces::msg::Time(), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("a", "d", builtin_interfaces::msg::Time(100), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("a", "d", builtin_interfaces::msg::Time(1000), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "d", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "d", tf2_ros::toMsg(tf2::timeFromSec(100)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("a", "d", tf2_ros::toMsg(tf2::timeFromSec(1000)), tf2::durationFromSec(1.0)));
 
   // Publish new transform with child 'd', should replace old one in static tf
   ts.header.frame_id = "new_parent";
@@ -106,24 +127,32 @@ TEST(StaticTransformPublisher, multiple_parent_test)
   ts.child_frame_id = "other_child2";
   stb.sendTransform(ts);
 
-  EXPECT_TRUE(mB.canTransform("new_parent", "d", builtin_interfaces::msg::Time(), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("new_parent", "other_child", builtin_interfaces::msg::Time(), tf2::durationFromSec(1.0)));
-  EXPECT_TRUE(mB.canTransform("new_parent", "other_child2", builtin_interfaces::msg::Time(), tf2::durationFromSec(1.0)));
-  EXPECT_FALSE(mB.canTransform("a", "d", builtin_interfaces::msg::Time(), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("new_parent", "d", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("new_parent", "other_child", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(1.0)));
+  EXPECT_TRUE(mB.canTransform("new_parent", "other_child2", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(1.0)));
+  EXPECT_FALSE(mB.canTransform("a", "d", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(1.0)));
+
+  rclcpp::shutdown();
 };
 
 TEST(StaticTransformPublisher, tf_from_param_server_valid)
 {
   // This TF is loaded from the parameter server; ensure it is valid.
-  tf2_ros::Buffer mB;
-  tf2_ros::TransformListener tfl(mB);
-  EXPECT_TRUE(mB.canTransform("robot_calibration", "world", ros::Time(), ros::Duration(1.0)));
-  EXPECT_TRUE(mB.canTransform("robot_calibration", "world", ros::Time(100), ros::Duration(1.0)));
-  EXPECT_TRUE(mB.canTransform("robot_calibration", "world", ros::Time(1000), ros::Duration(1.0)));
+  auto node = rclcpp::Node::make_shared("StaticTransformPublisher_tf_from_param_server_valid_test");
+
+  rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
+  tf2_ros::Buffer mB(clock);
+  tf2_ros::TransformListener tfl(mB, node, false);
+
+  EXPECT_TRUE(mB.canTransform("robot_calibration", "world", tf2_ros::toMsg(tf2::timeFromSec(0)), tf2::durationFromSec(1)));
+  EXPECT_TRUE(mB.canTransform("robot_calibration", "world", tf2_ros::toMsg(tf2::timeFromSec(100)), tf2::durationFromSec(1)));
+  EXPECT_TRUE(mB.canTransform("robot_calibration", "world", tf2_ros::toMsg(tf2::timeFromSec(1000)), tf2::durationFromSec(1)));
+
+  rclcpp::shutdown();
 }
 
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "tf_unittest");
+  rclcpp::init(argc, argv);
   return RUN_ALL_TESTS();
 }
