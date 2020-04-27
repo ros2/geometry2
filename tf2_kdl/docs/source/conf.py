e269adecce55ee029363f710c31d9f932b66c24f
ahcorde <ahcorde@gmail.com>
2020-04-27 15:00:54 +0200

    Added doxifiles and restructure
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- tf2_kdl/docs/source/conf.py
+++ tf2_kdl/docs/source/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import roslib
+#import roslib
 #roslib.load_manifest('tf2_kdl')
 import sys, os
 
@@ -24,7 +24,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.pngmath']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.imgmath']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
