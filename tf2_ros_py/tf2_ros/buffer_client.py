f9dab0cfc2f5cf5d64a40ffddbdab824940378b6
surfertas <tasuku@gmail.com>
2020-09-09 22:02:10 +0800

    Update tf2_ros_py/tf2_ros/buffer_client.py
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>

--- tf2_ros_py/tf2_ros/buffer_client.py
+++ tf2_ros_py/tf2_ros/buffer_client.py
@@ -154,8 +154,8 @@ class BufferClient(tf2_ros.BufferInterface):
         :param target_frame: Name of the frame to transform into.
         :param source_frame: Name of the input frame.
         :param time: The time at which to get the transform. (0 will get the latest)
-        :param timeout: (Optional) Time to wait for the target frame to become available.
-        :param return_debug_type: (Optional) If true, return a tuple representing debug information.
+        :param timeout: Time to wait for the target frame to become available.
+        :param return_debug_type: If true, return a tuple representing debug information.
         :return: True if the transform is possible, false otherwise.
         """
         try:
