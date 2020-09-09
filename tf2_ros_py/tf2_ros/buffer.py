d82ba023b60a08a196e0216b3e0bb9bb1f4f1686
surfertas <tasuku@gmail.com>
2020-09-09 21:56:27 +0800

    Update tf2_ros_py/tf2_ros/buffer.py
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>

--- tf2_ros_py/tf2_ros/buffer.py
+++ tf2_ros_py/tf2_ros/buffer.py
@@ -247,7 +247,7 @@ class Buffer(tf2.BufferCore, tf2_ros.BufferInterface):
         Must be implemented by a subclass of BufferInterface.
 
         :param target_frame: Name of the frame to transform into.
-        :param target_time: The time to transform to. (0 will get the latest)
+        :param target_time: The time to transform to (0 will get the latest).
         :param source_frame: Name of the input frame.
         :param source_time: The time at which source_frame will be evaluated. (0 will get the latest)
         :param fixed_frame: Name of the frame to consider constant in time.
