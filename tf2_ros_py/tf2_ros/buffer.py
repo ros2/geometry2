2b2f91a6499cebba5494f7b4167457dc30d3e205
surfertas <tasuku@gmail.com>
2020-09-09 21:58:38 +0800

    Update tf2_ros_py/tf2_ros/buffer.py
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>

--- tf2_ros_py/tf2_ros/buffer.py
+++ tf2_ros_py/tf2_ros/buffer.py
@@ -251,8 +251,8 @@ class Buffer(tf2.BufferCore, tf2_ros.BufferInterface):
         :param source_frame: Name of the input frame.
         :param source_time: The time at which source_frame will be evaluated (0 will get the latest).
         :param fixed_frame: Name of the frame to consider constant in time.
-        :param timeout: (Optional) Time to wait for the target frame to become available.
-        :param return_debug_type: (Optional) If true, return a tuple representing debug information.
+        :param timeout: Time to wait for the target frame to become available.
+        :param return_debug_type: If true, return a tuple representing debug information.
         :return: True if the transform is possible, false otherwise.
         """
         clock = rclpy.clock.Clock()
