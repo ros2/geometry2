44fc36486ed2429cc2a3a188c55f513709b066dc
surfertas <tasuku@gmail.com>
2020-09-12 17:49:05 +0800

    handle None for qos argument

--- tf2_ros_py/tf2_ros/transform_broadcaster.py
+++ tf2_ros_py/tf2_ros/transform_broadcaster.py
@@ -58,6 +58,7 @@ class TransformBroadcaster:
         """
         if qos is None:
             qos = QoSProfile(depth=100)
+            
         self.pub_tf = node.create_publisher(TFMessage, "/tf", qos)
 
     def sendTransform(
