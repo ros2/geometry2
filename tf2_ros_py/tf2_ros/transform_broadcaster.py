5abb94084447939d959d07189f9c9024bd6643eb
surfertas <tasuku@gmail.com>
2020-09-12 17:44:40 +0800

    handle None for qos argument

--- tf2_ros_py/tf2_ros/transform_broadcaster.py
+++ tf2_ros_py/tf2_ros/transform_broadcaster.py
@@ -43,18 +43,27 @@ class TransformBroadcaster:
     """
     :class:`TransformBroadcaster` is a convenient way to send transformation updates on the ``"/tf"`` message topic.
     """
-    def __init__(self, node: Node, qos: Union[QoSProfile, int] = QoSProfile(depth=100)) -> None:
+    def __init__(
+        self,
+        node: Node,
+        qos: Optional[Union[QoSProfile, int]] = None
+    ) -> None:
         """
-        .. function:: __init__(node, qos=QoSProfile(depth=100))
+        .. function:: __init__(node, qos=None)
 
             Constructor.
 
             :param node: The ROS2 node.
             :param qos: A QoSProfile or a history depth to apply to the publisher.
         """
+        if qos is None:
+            qos = QoSProfile(depth=100)
         self.pub_tf = node.create_publisher(TFMessage, "/tf", qos)
 
-    def sendTransform(self, transform: Union[TransformStamped, List[TransformStamped]]) -> None:
+    def sendTransform(
+        self,
+        transform: Union[TransformStamped, List[TransformStamped]]
+    ) -> None:
         """
         Send a transform, or a list of transforms, to the Buffer associated with this TransformBroadcaster.
 
