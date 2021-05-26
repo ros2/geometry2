296861827c1d5a50dfd8c3a71864223b18ea925d
Bjar Ne <gleichdick@users.noreply.github.com>
2021-05-26 14:22:17 +0000

    add forward.h

---
+++ tf2/include/tf2/impl/forward.hpp
@@ -0,0 +1,86 @@
+// Copyright 2021, Bjarne von Horn. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Open Source Robotics Foundation nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+/** \author Bjarne von Horn */
+
+#ifndef TF2__IMPL__FORWARD_HPP_
+#define TF2__IMPL__FORWARD_HPP_
+
+#include <array>
+
+namespace tf2
+{
+namespace impl
+{
+template<class Datatype, class = void>
+struct DefaultMessageForDatatype;
+template<class Datatype, class = void>
+struct DefaultTransformType;
+}  // namespace impl
+
+template<typename T>
+class Stamped;
+
+template<typename T>
+class WithCovarianceStamped;
+
+template<typename A, typename B>
+B & toMsg(const A & a, B & b);
+
+template<typename A, typename B = typename impl::DefaultMessageForDatatype<A>::type>
+B toMsg(const A & a);
+
+template<typename A, typename B>
+void fromMsg(const A & a, B & b);
+
+std::array<std::array<double, 6>, 6> covarianceRowMajorToNested(
+  const std::array<double, 36> & row_major);
+std::array<double, 36> covarianceNestedToRowMajor(
+  const std::array<std::array<double, 6>, 6> & nested_array);
+
+namespace impl
+{
+template<class Datatype, class Message, class = void>
+struct ConversionImplementation;
+
+template<class StampedMessage>
+struct StampedMessageTraits;
+
+template<class UnstampedMessage>
+struct UnstampedMessageTraits;
+
+template<typename T, int = 0>
+struct StampedAttributesHelper;
+
+template<bool IS_MESSAGE_A, bool IS_MESSAGE_B, typename A, typename B, typename = void>
+class Converter;
+
+}  // namespace impl
+}  // namespace tf2
+
+#endif  // TF2__IMPL__FORWARD_H_
