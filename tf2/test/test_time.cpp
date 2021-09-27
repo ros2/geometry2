17e9759eb632b34a3fa39b042816779b2ebef3b8
Chris Lalancette <clalancette@openrobotics.org>
2021-09-27 18:36:00 +0000

    Move all time tests into test_time.cpp
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- tf2/test/test_time.cpp
+++ tf2/test/test_time.cpp
@@ -33,7 +33,8 @@
 
 using namespace std::literals::chrono_literals;
 
-TEST(TestTime, durationFromSec) {
+TEST(TestTime, durationFromSec)
+{
   EXPECT_EQ(tf2::durationFromSec(0.0), 0ns);
   EXPECT_EQ(tf2::durationFromSec(-0.0), 0ns);
   EXPECT_EQ(tf2::durationFromSec(1.0), 1e9ns);
@@ -50,7 +51,8 @@ TEST(TestTime, durationFromSec) {
   EXPECT_EQ(tf2::durationFromSec(-1e-40), 0s);
 }
 
-TEST(TestTime, timeFromSec) {
+TEST(TestTime, timeFromSec)
+{
   auto time_point = tf2::TimePoint();
   EXPECT_EQ(tf2::timeFromSec(0.0), time_point);
   EXPECT_EQ(tf2::timeFromSec(1.0), time_point + 1s);
@@ -60,7 +62,8 @@ TEST(TestTime, timeFromSec) {
   EXPECT_EQ(tf2::timeFromSec(-1e-9), time_point - 1ns);
 }
 
-TEST(TestTime, durationToSec) {
+TEST(TestTime, durationToSec)
+{
   EXPECT_EQ(tf2::durationToSec(0s), 0.0);
   EXPECT_EQ(tf2::durationToSec(1s), 1.0);
   EXPECT_EQ(tf2::durationToSec(-1s), -1.0);
@@ -78,7 +81,8 @@ TEST(TestTime, durationToSec) {
   EXPECT_EQ(tf2::durationToSec(-2s - 500ms), -2.5);
 }
 
-TEST(TestTime, timeToSec) {
+TEST(TestTime, timeToSec)
+{
   auto time_point = tf2::TimePoint();
   EXPECT_EQ(tf2::timeToSec(time_point), 0.0);
   EXPECT_EQ(tf2::timeToSec(time_point + 1s), 1.0);
@@ -86,7 +90,8 @@ TEST(TestTime, timeToSec) {
   EXPECT_EQ(tf2::timeToSec(time_point + 1ns), 0.000000001);
 }
 
-TEST(TestTime, displayTimePoint) {
+TEST(TestTime, displayTimePoint)
+{
   EXPECT_EQ(tf2::displayTimePoint(tf2::TimePoint(0s)), "0.000000");
   EXPECT_EQ(tf2::displayTimePoint(tf2::TimePoint(-0s)), "0.000000");
   EXPECT_EQ(tf2::displayTimePoint(tf2::TimePoint(1s)), "1.000000");
@@ -97,4 +102,63 @@ TEST(TestTime, displayTimePoint) {
   EXPECT_EQ(tf2::displayTimePoint(tf2::TimePoint(-1ns)), "-0.000000");
   EXPECT_EQ(tf2::displayTimePoint(tf2::TimePoint(1000ns)), "0.000001");
   EXPECT_EQ(tf2::displayTimePoint(tf2::TimePoint(-1000ns)), "-0.000001");
+  EXPECT_EQ(tf2::displayTimePoint(tf2::TimePoint(4ms)), "0.004000");
+}
+
+TEST(TestTime, To_From_Sec)
+{
+  // Exact representation of a time.
+  tf2::TimePoint t1 = tf2::get_now();
+
+  // Approximate representation of the time in seconds as a double (floating point
+  // error introduced).
+  double t1_sec = tf2::timeToSec(t1);
+
+  // Time point from the t1_sec approximation.
+  tf2::TimePoint t2 = tf2::timeFromSec(t1_sec);
+
+  // Check that the difference due to t1_sec being approximate is small.
+  tf2::Duration diff = t2 > t1 ? t2 - t1 : t1 - t2;
+  EXPECT_TRUE(diff < tf2::Duration(std::chrono::nanoseconds(200)));
+
+  // No new floating point errors are expected after converting to and from time points.
+  double t2_sec = tf2::timeToSec(t2);
+  EXPECT_EQ(t1_sec, t2_sec);
+  EXPECT_EQ(tf2::timeFromSec(t1_sec), tf2::timeFromSec(t2_sec));
+}
+
+TEST(TestTime, To_From_Duration)
+{
+  tf2::TimePoint t1 = tf2::get_now();
+
+  std::vector<double> values = {
+    -0.01, -0.2, -0.5, -0.7, -0.99, -5.7, -1000000, -123456789.123456789,
+    0.01, 0.2, 0.5, 0.7, 0.99, 5.7, 10000000, 123456789.123456789,
+    0.0,
+  };
+
+  for (double expected_diff_sec : values) {
+    tf2::TimePoint t2 = tf2::timeFromSec(tf2::timeToSec(t1) + expected_diff_sec);
+
+    // Check durationToSec.
+    tf2::Duration duration1 = t2 - t1;
+
+    // Approximate representation of the duration in seconds as a double (floating point
+    // error introduced).
+    double duration1_sec = tf2::durationToSec(duration1);
+
+    // Check that the difference due to duration_sec being approximate is small.
+    double error_sec = duration1_sec - expected_diff_sec;
+    EXPECT_TRUE(std::abs(error_sec) < 200 * 1e-9);
+
+    // Check durationFromSec.
+    tf2::Duration duration2 = tf2::durationFromSec(tf2::durationToSec(duration1));
+    tf2::Duration error_duration = duration1 - duration2;
+    // Get the absolute difference between Durations.
+    error_duration = error_duration > tf2::Duration(0) ? error_duration : -error_duration;
+    // Increased tolerance for larger differences.
+    int32_t tol_ns = std::abs(expected_diff_sec) > 100000 ? 10 : 1;
+
+    EXPECT_TRUE(error_duration < tf2::Duration(std::chrono::nanoseconds(tol_ns)));
+  }
 }
