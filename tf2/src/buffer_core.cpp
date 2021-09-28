4418a1e0b1cc8838aca35bc0bf745e105bd3e470
Chris Lalancette <clalancette@openrobotics.org>
2021-09-28 14:45:55 +0000

    Remove totally unused (and unreachable) code.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- tf2/src/buffer_core.cpp
+++ tf2/src/buffer_core.cpp
@@ -55,79 +55,11 @@
 namespace tf2
 {
 
+namespace
+{
+
 // Tolerance for acceptable quaternion normalization
 constexpr static double QUATERNION_NORMALIZATION_TOLERANCE = 10e-3;
-
-/** \brief convert Transform msg to Transform */
-void transformMsgToTF2(const geometry_msgs::msg::Transform & msg, tf2::Transform & tf2)
-{
-  tf2 =
-    tf2::Transform(
-    tf2::Quaternion(
-      msg.rotation.x, msg.rotation.y, msg.rotation.z,
-      msg.rotation.w),
-    tf2::Vector3(msg.translation.x, msg.translation.y, msg.translation.z));
-}
-
-/** \brief convert Transform to Transform msg*/
-void transformTF2ToMsg(const tf2::Transform & tf2, geometry_msgs::msg::Transform & msg)
-{
-  msg.translation.x = tf2.getOrigin().x();
-  msg.translation.y = tf2.getOrigin().y();
-  msg.translation.z = tf2.getOrigin().z();
-  msg.rotation.x = tf2.getRotation().x();
-  msg.rotation.y = tf2.getRotation().y();
-  msg.rotation.z = tf2.getRotation().z();
-  msg.rotation.w = tf2.getRotation().w();
-}
-
-/** \brief convert Transform to Transform msg*/
-void transformTF2ToMsg(
-  const tf2::Transform & tf2, geometry_msgs::msg::TransformStamped & msg,
-  builtin_interfaces::msg::Time stamp, const std::string & frame_id,
-  const std::string & child_frame_id)
-{
-  transformTF2ToMsg(tf2, msg.transform);
-  msg.header.stamp = stamp;
-  msg.header.frame_id = frame_id;
-  msg.child_frame_id = child_frame_id;
-}
-
-void transformTF2ToMsg(
-  const tf2::Quaternion & orient, const tf2::Vector3 & pos,
-  geometry_msgs::msg::Transform & msg)
-{
-  msg.translation.x = pos.x();
-  msg.translation.y = pos.y();
-  msg.translation.z = pos.z();
-  msg.rotation.x = orient.x();
-  msg.rotation.y = orient.y();
-  msg.rotation.z = orient.z();
-  msg.rotation.w = orient.w();
-}
-
-void transformTF2ToMsg(
-  const tf2::Quaternion & orient, const tf2::Vector3 & pos,
-  geometry_msgs::msg::TransformStamped & msg,
-  builtin_interfaces::msg::Time stamp, const std::string & frame_id,
-  const std::string & child_frame_id)
-{
-  transformTF2ToMsg(orient, pos, msg.transform);
-  msg.header.stamp = stamp;
-  msg.header.frame_id = frame_id;
-  msg.child_frame_id = child_frame_id;
-}
-
-void setIdentity(geometry_msgs::msg::Transform & tx)
-{
-  tx.translation.x = 0;
-  tx.translation.y = 0;
-  tx.translation.z = 0;
-  tx.rotation.x = 0;
-  tx.rotation.y = 0;
-  tx.rotation.z = 0;
-  tx.rotation.w = 1;
-}
 
 bool startsWithSlash(const std::string & frame_id)
 {
@@ -147,9 +79,6 @@ std::string stripSlash(const std::string & in)
   }
   return out;
 }
-
-namespace
-{
 
 void fillOrWarnMessageForInvalidFrame(
   const char * function_name_arg,
