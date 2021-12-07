3e76762a790cb8579dca1b1d5c4129e9e1864990
Aaron Chong <aaronchongth@gmail.com>
2021-12-07 15:00:07 +0800

    Added additinoal test for resetting a unique ptr of a buffer
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- tf2_ros/test/test_buffer.cpp
+++ tf2_ros/test/test_buffer.cpp
@@ -330,6 +330,43 @@ TEST(test_buffer, wait_for_transform_buffer_destructed_race)
   EXPECT_EQ(status, std::future_status::timeout);
 }
 
+TEST(test_buffer, wait_for_transform_buffer_reset_race)
+{
+  rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
+  bool callback_timeout = false;
+  tf2_ros::TransformStampedFuture future;
+
+  std::unique_ptr<tf2_ros::Buffer> buffer(new tf2_ros::Buffer(clock));
+  ASSERT_NE(nullptr, buffer);
+  // Silence error about dedicated thread's being necessary
+  buffer->setUsingDedicatedThread(true);
+  auto mock_create_timer = std::make_shared<MockCreateTimer>();
+  buffer->setCreateTimerInterface(mock_create_timer);
+
+  rclcpp::Time rclcpp_time = clock->now();
+  tf2::TimePoint tf2_time(std::chrono::nanoseconds(rclcpp_time.nanoseconds()));
+
+  future = buffer->waitForTransform(
+    "foo",
+    "bar",
+    tf2_time, tf2::durationFromSec(1.0),
+    [&callback_timeout](const tf2_ros::TransformStampedFuture & future)
+    {
+      try {
+        // We don't expect this throw, even though a timeout will occur
+        future.get();
+      } catch (...) {
+        callback_timeout = true;
+      }
+    });
+
+  buffer.reset();
+  ASSERT_EQ(nullptr, buffer);
+
+  auto status = future.wait_for(std::chrono::seconds(1));
+  EXPECT_EQ(status, std::future_status::timeout);
+}
+
 int main(int argc, char ** argv)
 {
   testing::InitGoogleTest(&argc, argv);
