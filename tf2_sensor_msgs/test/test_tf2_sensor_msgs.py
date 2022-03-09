7f91b568399c7f5f18071876ac2a6df865e744ae
Florian Vahl <git@flova.de>
2022-03-10 00:50:04 +0100

    Cleanup tests
    Signed-off-by: Florian Vahl <git@flova.de>

--- tf2_sensor_msgs/test/test_tf2_sensor_msgs.py
+++ tf2_sensor_msgs/test/test_tf2_sensor_msgs.py
@@ -29,61 +29,55 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 import copy
-import struct
 import unittest
 
-from sensor_msgs import point_cloud2
-from sensor_msgs.msg import PointField
+import numpy as np
+from sensor_msgs_py.point_cloud2 import create_cloud_xyz32, read_points
+from std_msgs.msg import Header
 from tf2_ros import TransformStamped
-import tf2_sensor_msgs
+from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
 
 
-# A sample python unit test
 class PointCloudConversions(unittest.TestCase):
 
     def setUp(self):
-        self.point_cloud_in = point_cloud2.PointCloud2()
-        self.point_cloud_in.fields = [
-            PointField('x', 0, PointField.FLOAT32, 1),
-            PointField('y', 4, PointField.FLOAT32, 1),
-            PointField('z', 8, PointField.FLOAT32, 1)]
+        self.points = np.array([[1, 2, 0], [10, 20, 30]], dtype=np.float32)
 
-        self.point_cloud_in.point_step = 4 * 3
-        self.point_cloud_in.height = 1
-        # we add two points (with x, y, z to the cloud)
-        self.point_cloud_in.width = 2
-        self.point_cloud_in.row_step = \
-            self.point_cloud_in.point_step * self.point_cloud_in.width
-
-        points = [1, 2, 0, 10, 20, 30]
-        self.point_cloud_in.data = struct.pack('%sf' % len(points), *points)
-
-        self.transform_translate_xyz_300 = TransformStamped()
-        self.transform_translate_xyz_300.transform.translation.x = 300
-        self.transform_translate_xyz_300.transform.translation.y = 300
-        self.transform_translate_xyz_300.transform.translation.z = 300
-        # no rotation so we only set w
-        self.transform_translate_xyz_300.transform.rotation.w = 1
-
-        assert(list(point_cloud2.read_points(self.point_cloud_in)) ==
-               [(1.0, 2.0, 0.0), (10.0, 20.0, 30.0)])
+        self.point_cloud_in = create_cloud_xyz32(
+            Header(frame_id="test"),
+            self.points)
 
     def test_simple_transform(self):
+        k = 300.0
+        transform_translate_xyz = TransformStamped()
+        transform_translate_xyz.transform.translation.x = k
+        transform_translate_xyz.transform.translation.y = k
+        transform_translate_xyz.transform.translation.z = k
+        # no rotation so we only set w
+        transform_translate_xyz.transform.rotation.w = 1.0
+
+        self.assertEqual(
+            list(read_points(self.point_cloud_in)),
+            [(1.0, 2.0, 0.0), (10.0, 20.0, 30.0)])
+
         # deepcopy is not required here because we have a str for now
         old_data = copy.deepcopy(self.point_cloud_in.data)
-        point_cloud_transformed = tf2_sensor_msgs.do_transform_cloud(
-            self.point_cloud_in, self.transform_translate_xyz_300)
 
-        k = 300
-        expected_coordinates = [(1+k, 2+k, 0+k), (10+k, 20+k, 30+k)]
-        new_points = list(point_cloud2.read_points(point_cloud_transformed))
-        print('new_points are %s' % new_points)
-        assert(expected_coordinates == new_points)
+        # Apply transform
+        point_cloud_transformed = do_transform_cloud(
+            self.point_cloud_in, transform_translate_xyz)
+
+        # Expected output
+        expected_coordinates = self.points + k
+
+        new_points = list(map(list, read_points(point_cloud_transformed)))
+        self.assertEqual(expected_coordinates.tolist(), new_points)
         # checking no modification in input cloud
-        assert(old_data == self.point_cloud_in.data)
+        self.assertEqual(old_data, self.point_cloud_in.data)
+
+    def test_simple_rotation_transform(self):
+        pass
 
 
 if __name__ == '__main__':
-    import rosunit
-    rosunit.unitrun('test_tf2_sensor_msgs', 'test_point_cloud_conversion',
-                    PointCloudConversions)
+    unittest.main()
