55a7534dc3dfde257fb5d1b6cb09b8ecafe23be1
Florian Vahl <git@flova.de>
2022-03-10 00:50:43 +0100

    Format code
    Signed-off-by: Florian Vahl <git@flova.de>

--- tf2_sensor_msgs/tf2_sensor_msgs/tf2_sensor_msgs.py
+++ tf2_sensor_msgs/tf2_sensor_msgs/tf2_sensor_msgs.py
@@ -29,9 +29,10 @@
 import numpy as np
 import numpy.typing as npt
 import tf2_ros
-from geometry_msgs.msg import Transform
+from geometry_msgs.msg import Transform, TransformStamped
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs_py.point_cloud2 import create_cloud, read_points
+from typing import Union
 
 
 def to_msg_msg(msg):
@@ -48,14 +49,20 @@ def from_msg_msg(msg):
 tf2_ros.ConvertRegistration().add_from_msg(PointCloud2, from_msg_msg)
 
 
-def transform_points(point_cloud: npt.ArrayLike, transform: Transform) -> np.ndarray:
+def transform_points(
+        point_cloud: npt.ArrayLike,
+        transform: Union[Transform, TransformStamped]) -> np.ndarray:
     """
-    Transforms a bulk of points from an numpy array using a provided `Transform`.
+    Transform a bulk of points from an numpy array using a provided `Transform`.
 
     :param point_cloud: nx3 Array of points where n is the number of points
     :param transform: TF2 transform used for the transformation
     :returns: Array with the same shape as the input array, but with the transformation applied
     """
+    # Check if we have a TransformStamped
+    if isinstance(transform, TransformStamped):
+        transform = transform.transform
+
     # Cast ArrayLike object to np.ndarray
     point_cloud = np.asarray(point_cloud)
 
@@ -74,22 +81,33 @@ def transform_points(point_cloud: npt.ArrayLike, transform: Transform) -> np.nda
         ]))
 
     # "Batched" matmul meaning a matmul for each point
-    # First we offset all points by the translation part followed by a rotation using the rotation matrix
-    return np.einsum('ij, pj -> pi', transform_rotation_matrix, point_cloud + transform_translation)
+    # First we offset all points by the translation part
+    # followed by a rotation using the rotation matrix
+    return np.einsum(
+        'ij, pj -> pi',
+        transform_rotation_matrix,
+        point_cloud + transform_translation)
 
 
-
-def do_transform_cloud(cloud: PointCloud2, transform: Transform) -> PointCloud2:
+def do_transform_cloud(
+        cloud: PointCloud2,
+        transform: Union[Transform, TransformStamped]) -> PointCloud2:
     """
-    Applies a `Transform` on a `PointCloud2`.
+    Apply a `Transform` or `TransformStamped` on a `PointCloud2`.
+
+    The x, y, and z values are transformed into a different frame,
+    while the rest of the cloud is kept untouched.
 
     :param cloud: The point cloud that should be transformed
     :param transform: The transform which will applied to the point cloud
     :returns: The transformed point cloud
     """
-    points = read_points(cloud)
-    points_out = transform_points(points, transform)
-    return create_cloud(transform.header, cloud.fields, points_out)
+    points = list(read_points(  # TODO remove list and use numpy method
+        cloud,
+        field_names=["x", "y", "z"]))
+    points = transform_points(points, transform)  # TODO set points[["x", "y", "z"]]
+    # TODO add points to the rest of the points
+    return create_cloud(transform.header, cloud.fields, points)
 
 
 tf2_ros.TransformRegistration().add(PointCloud2, do_transform_cloud)
@@ -97,17 +115,18 @@ tf2_ros.TransformRegistration().add(PointCloud2, do_transform_cloud)
 
 def _get_mat_from_quat(quaternion: np.ndarray) -> np.ndarray:
     """
-    Converts a quaternion to a rotation matrix
+    Convert a quaternion to a rotation matrix.
 
     This method is currently needed because transforms3d is not released as a `.dep` and
     would require user interaction to set up.
 
-    For reference see: https://github.com/matthew-brett/transforms3d/blob/f185e866ecccb66c545559bc9f2e19cb5025e0ab/transforms3d/quaternions.py#L101
+    For reference see: https://github.com/matthew-brett/transforms3d/blob/
+    f185e866ecccb66c545559bc9f2e19cb5025e0ab/transforms3d/quaternions.py#L101
 
     :param quaternion: A numpy array containing the w, x, y, and z components of the quaternion
     :returns: An array containing an X, Y, and Z translation component
     """
-    Nq = np.sum(np.square(quaternion, 2))
+    Nq = np.sum(np.square(quaternion))
     if Nq < np.finfo(np.float64).eps:
         return np.eye(3)
 
@@ -118,6 +137,6 @@ def _get_mat_from_quat(quaternion: np.ndarray) -> np.ndarray:
     zZ = XYZ[2] * quaternion[3]
 
     return np.array(
-        [[ 1.0-(yYZ[0]+zZ), xXYZ[1]-wXYZ[2], xXYZ[2]+wXYZ[1]],
-        [ xXYZ[1]+wXYZ[2], 1.0-(xXYZ[0]+zZ), yYZ[1]-wXYZ[0]],
-        [ xXYZ[2]-wXYZ[1], yYZ[1]+wXYZ[0], 1.0-(xXYZ[0]+yYZ[0])]])
+        [[1.0-(yYZ[0]+zZ), xXYZ[1]-wXYZ[2], xXYZ[2]+wXYZ[1]],
+         [xXYZ[1]+wXYZ[2], 1.0-(xXYZ[0]+zZ), yYZ[1]-wXYZ[0]],
+         [xXYZ[2]-wXYZ[1], yYZ[1]+wXYZ[0], 1.0-(xXYZ[0]+yYZ[0])]])
