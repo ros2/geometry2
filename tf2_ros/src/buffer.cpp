00fa84285691893c32786e1113b00aad81ebe2c6
Jacob Perron <jacob@openrobotics.org>
2022-06-20 08:33:58 -0700

    Suppress console spam in other calls to canTransform
    The user has few options to workaround potential console spam from canTransform.
    If we want, it would be better to update the API to optional return the error string.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- tf2_ros/src/buffer.cpp
+++ tf2_ros/src/buffer.cpp
@@ -91,7 +91,9 @@ Buffer::lookupTransform(
   const std::string & target_frame, const std::string & source_frame,
   const tf2::TimePoint & lookup_time, const tf2::Duration timeout) const
 {
-  canTransform(target_frame, source_frame, lookup_time, timeout);
+  // Pass error string to suppress console spam
+  std::string error;
+  canTransform(target_frame, source_frame, lookup_time, timeout, &error);
   return lookupTransform(target_frame, source_frame, lookup_time);
 }
 
@@ -114,7 +116,9 @@ Buffer::lookupTransform(
   const std::string & source_frame, const tf2::TimePoint & source_time,
   const std::string & fixed_frame, const tf2::Duration timeout) const
 {
-  canTransform(target_frame, target_time, source_frame, source_time, fixed_frame, timeout);
+  // Pass error string to suppress console spam
+  std::string error;
+  canTransform(target_frame, target_time, source_frame, source_time, fixed_frame, timeout, &error);
   return lookupTransform(target_frame, target_time, source_frame, source_time, fixed_frame);
 }
 
