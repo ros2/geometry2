2d0ef227309d3024c0decb20d0457458ab7335ac
Charles Dawson <charles.dwsn@gmail.com>
2022-10-04 14:02:14 -0600

    Drop PyKDL dependency in tf2_geometry_msgs (backport #509 to foxy) (#532)
    * Drop PyKDL dependency in tf2_geometry_msgs (#509)
    
    * Drop PyKDL dependency
    
    Signed-off-by: Florian Vahl <git@flova.de>
    Co-authored-by: Chris Lalancette <clalancette@openrobotics.org>
    (cherry picked from commit 6b18a40056708c6f3acaafc08352b14554e33962)
    
    # Conflicts:
    #	tf2_geometry_msgs/CMakeLists.txt
    #	tf2_geometry_msgs/package.xml
    #	tf2_geometry_msgs/src/tf2_geometry_msgs/tf2_geometry_msgs.py
    #	tf2_geometry_msgs/test/test_tf2_geometry_msgs.py
    #	tf2_ros_py/package.xml
    
    * Clean up merge conflicts. Builds and tests pass!
    
    Co-authored-by: Florian Vahl <florian@flova.de>

--- tf2_geometry_msgs/test/test_tf2_geometry_msgs.py
+++ tf2_geometry_msgs/test/test_tf2_geometry_msgs.py
@@ -1,11 +1,38 @@
-#!/usr/bin/env python3
+# Copyright 2008 Willow Garage, Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the Willow Garage, Inc. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 import unittest
 import rclpy
-import PyKDL
 import tf2_ros
 import tf2_geometry_msgs
 from geometry_msgs.msg import TransformStamped, PointStamped, Vector3Stamped, PoseStamped, PoseWithCovarianceStamped, Quaternion
+import numpy as np
+
 
 class GeometryMsgs(unittest.TestCase):
     def test_transform(self):
@@ -66,7 +93,6 @@ class GeometryMsgs(unittest.TestCase):
         self.assertEqual(out.pose.pose.position.x, 0)
         self.assertEqual(out.pose.pose.position.y, -2)
         self.assertEqual(out.pose.pose.position.z, -3)
-        self.assertEqual(out.pose.covariance, v.pose.covariance)
 
         # Translation shouldn't affect Vector3
         t = TransformStamped()
@@ -100,6 +126,47 @@ class GeometryMsgs(unittest.TestCase):
         self.assertEqual(out.vector.y, 0)
         self.assertEqual(out.vector.z, 0)
 
+        # Testing for pose and covariance transform
+        t = TransformStamped()
+        t.transform.translation.x = 1.0
+        t.transform.translation.y = 2.0
+        t.transform.translation.z = 3.0
+        t.transform.rotation = Quaternion(w=0.0, x=1.0, y=0.0, z=0.0)
+
+        v = PoseWithCovarianceStamped()
+        v.header.stamp = rclpy.time.Time(seconds=2.0).to_msg()
+        v.header.frame_id = 'a'
+        v.pose.covariance = (
+          1.0, 2.0, 3.0, 4.0, 5.0, 6.0,
+          1.0, 2.0, 3.0, 4.0, 5.0, 6.0,
+          1.0, 2.0, 3.0, 4.0, 5.0, 6.0,
+          1.0, 2.0, 3.0, 4.0, 5.0, 6.0,
+          1.0, 2.0, 3.0, 4.0, 5.0, 6.0,
+          1.0, 2.0, 3.0, 4.0, 5.0, 6.0
+        )
+        v.pose.pose.position.x = 1.0
+        v.pose.pose.position.y = 2.0
+        v.pose.pose.position.z = 3.0
+        v.pose.pose.orientation = Quaternion(w=0.0, x=1.0, y=0.0, z=0.0)
+
+        out = tf2_geometry_msgs.do_transform_pose_with_covariance_stamped(v, t)
+        expected_covariance = np.array([
+          1.0, -2.0, -3.0, 4.0, -5.0, -6.0,
+          -1.0, 2.0, 3.0, -4.0, 5.0, 6.0,
+          -1.0, 2.0, 3.0, -4.0, 5.0, 6.0,
+          1.0, -2.0, -3.0, 4.0, -5.0, -6.0,
+          -1.0, 2.0, 3.0, -4.0, 5.0, 6.0,
+          -1.0, 2.0, 3.0, -4.0, 5.0, 6.0
+        ])
+        self.assertEqual(out.pose.pose.position.x, 2)
+        self.assertEqual(out.pose.pose.position.y, 0)
+        self.assertEqual(out.pose.pose.position.z, 0)
+        self.assertEqual(out.pose.pose.orientation.x, 0)
+        self.assertEqual(out.pose.pose.orientation.y, 0)
+        self.assertEqual(out.pose.pose.orientation.z, 0)
+        self.assertEqual(out.pose.pose.orientation.w, 1)
+        self.assertTrue(np.array_equal(out.pose.covariance, expected_covariance))
+
+
 if __name__ == '__main__':
     rclpy.init(args=None)
-    unittest.main()
