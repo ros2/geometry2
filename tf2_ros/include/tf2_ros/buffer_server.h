a8d461377d3b4f53081d4b8d286c89b7218e587d
Tony Najjar <tony.najjar@logivations.com>
2023-11-09 13:48:55 +0100

    Update tf2_ros/include/tf2_ros/buffer_server.h
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>
    Signed-off-by: Tony Najjar <tony.najjar.1997@gmail.com>

--- tf2_ros/include/tf2_ros/buffer_server.h
+++ tf2_ros/include/tf2_ros/buffer_server.h
@@ -140,7 +140,8 @@ private:
   geometry_msgs::msg::TransformStamped lookupTransform(GoalHandle gh);
 
   TF2_ROS_PUBLIC
-  geometry_msgs::msg::TransformStamped lookupTransform(const std::shared_ptr<LookupTransformService::Request> request);
+  geometry_msgs::msg::TransformStamped lookupTransform(
+    const std::shared_ptr<LookupTransformService::Request> request);
 
   const tf2::BufferCoreInterface & buffer_;
   rclcpp::Logger logger_;
