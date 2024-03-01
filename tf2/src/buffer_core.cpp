f3d26a07ba943df8c7f413f66cb45a9d3334a0f2
Lucas Wendland <82680922+CursedRock17@users.noreply.github.com>
2024-03-01 09:52:11 -0500

    Using uint_8 for retval logging

--- tf2/src/buffer_core.cpp
+++ tf2/src/buffer_core.cpp
@@ -677,7 +677,7 @@ void BufferCore::lookupTransformImpl(
       case tf2::TF2Error::TF2_LOOKUP_ERROR:
         throw LookupException(error_string);
       default:
-        RCUTILS_LOG_ERROR("Unknown error code: %d", retval);
+        RCUTILS_LOG_ERROR("Unknown error code: %u", static_cast<std::uint_8>(retval));
         assert(0);
     }
   }
@@ -1479,7 +1479,7 @@ void BufferCore::_chainAsVector(
       case tf2::TF2Error::TF2_LOOKUP_ERROR:
         throw LookupException(error_string);
       default:
-        RCUTILS_LOG_ERROR("Unknown error code: %d", retval);
+        RCUTILS_LOG_ERROR("Unknown error code: %u", static_cast<std::uint_8>(retval));
         assert(0);
     }
   }
@@ -1505,7 +1505,7 @@ void BufferCore::_chainAsVector(
         case tf2::TF2Error::TF2_LOOKUP_ERROR:
           throw LookupException(error_string);
         default:
-          RCUTILS_LOG_ERROR("Unknown error code: %d", retval);
+          RCUTILS_LOG_ERROR("Unknown error code: %u", static_cast<std::uint_8>(retval));
           assert(0);
       }
     }
