c2dd515a0d009ede193d354bce69c2baef457194
Eric Cousineau <eric.cousineau@tri.global>
2024-05-14 10:33:46 -0400

    review

--- tf2/test/cache_benchmark.cpp
+++ tf2/test/cache_benchmark.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024, Willow Garage, Inc. All rights reserved.
+// Copyright 2024, Open Source Robotics Foundation, Inc. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -10,9 +10,9 @@
 //      notice, this list of conditions and the following disclaimer in the
 //      documentation and/or other materials provided with the distribution.
 //
-//    * Neither the name of the Willow Garage nor the names of its
-//      contributors may be used to endorse or promote products derived from
-//      this software without specific prior written permission.
+//    * Neither the name of the Open Source Robotics Foundation, Inc. nor the
+//      names of its contributors may be used to endorse or promote products
+//      derived from this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -28,6 +28,7 @@
 
 #include <benchmark/benchmark.h>
 
+#include <tuple>
 #include <vector>
 
 #include "tf2/time_cache.h"
