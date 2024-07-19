3e6e9d312514c68bf816a884873bb83cca553020
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-07-19 09:37:11 +0200

    0.38.2

--- examples_tf2_py/setup.py
+++ examples_tf2_py/setup.py
@@ -4,7 +4,7 @@ package_name = 'examples_tf2_py'
 
 setup(
     name=package_name,
-    version='0.38.1',
+    version='0.38.2',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
--- tf2_tools/setup.py
+++ tf2_tools/setup.py
@@ -4,7 +4,7 @@ package_name = 'tf2_tools'
 
 setup(
     name=package_name,
-    version='0.38.1',
+    version='0.38.2',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
