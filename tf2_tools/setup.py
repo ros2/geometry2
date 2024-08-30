d0dbd516d1748cfe1b293e0f7d5b2f82ddd3438a
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-08-30 14:00:16 +0200

    0.25.8

--- tf2_tools/setup.py
+++ tf2_tools/setup.py
@@ -4,7 +4,7 @@ package_name = 'tf2_tools'
 
 setup(
     name=package_name,
-    version='0.25.7',
+    version='0.25.8',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
