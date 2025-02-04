3298edd03a58facf320bae23a8d979abe033a6f4
Emmanuel <EmmanuelMess@users.noreply.github.com>
2025-02-03 21:20:52 -0300

    Fix tf2_ros mapping
    Signed-off-by: Emmanuel <EmmanuelMess@users.noreply.github.com>

--- tf2_ros/doc/conf.py
+++ tf2_ros/doc/conf.py
@@ -215,9 +215,9 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'http://docs.python.org/': None,
-    'http://docs.opencv.org/3.0-last-rst/': None,
-    'http://docs.scipy.org/doc/numpy': None
+    "python": ('http://docs.python.org/3', None),
+    "opencv": ('http://docs.opencv.org/3.0-last-rst', None),
+    "numpy": ('http://docs.scipy.org/doc/numpy', None)
     }
 
 autoclass_content = "both"
