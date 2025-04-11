426a4daa79048da3504d39676c0bb433a46eac3f
Gilbert Tanner <gilberttanner.work@gmail.com>
2025-04-11 10:22:23 +0200

    Split imports into individual lines
    Co-authored-by: Christophe Bedard <bedard.christophe@gmail.com>
    Signed-off-by: Gilbert Tanner <gilberttanner.work@gmail.com>

--- tf2_sensor_msgs/tf2_sensor_msgs/__init__.py
+++ tf2_sensor_msgs/tf2_sensor_msgs/__init__.py
@@ -26,7 +26,8 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from .tf2_sensor_msgs import do_transform_cloud, transform_points
+from .tf2_sensor_msgs import do_transform_cloud
+from .tf2_sensor_msgs import transform_points
 
 __all__ = [
     'do_transform_cloud',
