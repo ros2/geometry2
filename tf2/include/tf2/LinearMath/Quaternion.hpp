2be7ac1aecd3d8a9194e7ff605a24a3813fe554c
Michael Carlstrom <rmc@carlstrom.com>
2025-04-24 16:10:35 -0400

    Add isnan support (#780)
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- tf2/include/tf2/LinearMath/Quaternion.hpp
+++ tf2/include/tf2/LinearMath/Quaternion.hpp
@@ -17,6 +17,7 @@ subject to the following restrictions:
 #ifndef TF2__LINEARMATH__QUATERNION_HPP_
 #define TF2__LINEARMATH__QUATERNION_HPP_
 
+#include <cmath>
 
 #include "Vector3.hpp"
 #include "QuadWord.hpp"
@@ -159,6 +160,10 @@ public:
 	tf2Scalar length() const
 	{
 		return tf2Sqrt(length2());
+	}
+
+	TF2SIMD_FORCE_INLINE bool isnan() const {
+		return std::isnan(m_floats[0]) || std::isnan(m_floats[1]) || std::isnan(m_floats[2]) || std::isnan(m_floats[3]);
 	}
 
   /**@brief Normalize the quaternion 
--- tf2/include/tf2/LinearMath/Vector3.hpp
+++ tf2/include/tf2/LinearMath/Vector3.hpp
@@ -17,6 +17,7 @@ subject to the following restrictions:
 #ifndef TF2__LINEARMATH__VECTOR3_HPP_
 #define TF2__LINEARMATH__VECTOR3_HPP_
 
+#include <cmath>
 
 #include "Scalar.hpp"
 #include "MinMax.hpp"
@@ -139,11 +140,11 @@ public:
 	}
 
   /**@brief Return the distance squared between the ends of this and another vector
-   * This is symantically treating the vector like a point */
+   * This is semantically treating the vector like a point */
 	TF2SIMD_FORCE_INLINE tf2Scalar distance2(const Vector3& v) const;
 
   /**@brief Return the distance between the ends of this and another vector
-   * This is symantically treating the vector like a point */
+   * This is semantically treating the vector like a point */
 	TF2SIMD_FORCE_INLINE tf2Scalar distance(const Vector3& v) const;
 
   /**@brief Normalize this vector 
@@ -336,6 +337,10 @@ public:
 		TF2SIMD_FORCE_INLINE bool fuzzyZero() const 
 		{
 			return length2() < TF2SIMD_EPSILON;
+		}
+
+		TF2SIMD_FORCE_INLINE bool isnan() const {
+			return std::isnan(m_floats[0]) || std::isnan(m_floats[1]) || std::isnan(m_floats[2]);
 		}
 
 		TF2SIMD_FORCE_INLINE	void	serialize(struct	Vector3Data& dataOut) const;
