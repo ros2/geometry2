36494b93d7d565d384f03a2173b90d46c79049d6
mosFet <realeandrea@yahoo.it>
2025-07-02 14:09:19 +0200

    Fix deprecation warning
    FIX:
    
    #UserWarning: Unknown distribution option: 'tests_require'
    
    and
    
    #SetuptoolsDeprecationWarning: License classifiers are deprecated. !!
    
        ********************************************************************************
        Please consider removing the following classifiers in favor of a SPDX license expression:
    
        License :: OSI Approved :: BSD License
    
        See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details.
        ********************************************************************************

--- tf2_ros_py/setup.py
+++ tf2_ros_py/setup.py
@@ -21,7 +21,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -29,5 +28,9 @@ setup(
         'This package contains the ROS Python bindings for the tf2 library.'
     ),
     license='BSD',
-    tests_require=['pytest'],
+    extras_require={
+        'test' : [
+            'pytest'
+        ]
+    },
 )
