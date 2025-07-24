d180c9d8f93274798dac553632bf6846dca81911
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-07-24 08:56:35 +0200

    Fix Setuptools deprecations (#809)
    (cherry picked from commit 1be1ae1b72497937a16aed2af316cfec1f52e59c)

--- examples_tf2_py/setup.py
+++ examples_tf2_py/setup.py
@@ -21,7 +21,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -29,7 +28,11 @@ setup(
         'Has examples of using the tf2 python api.'
     ),
     license='Apache License, Version 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'static_broadcaster = examples_tf2_py.static_broadcaster:main',
