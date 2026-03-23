9a326e1b517d90f8f24db5f02ceb48f9dd030b24
Alejandro Hernández Cordero <ahcorde@gmail.com>
2026-03-23 10:07:38 +0100

    fixed typoe in buffer.py (#905)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- tf2_ros_py/tf2_ros/buffer.py
+++ tf2_ros_py/tf2_ros/buffer.py
@@ -230,7 +230,7 @@ class Buffer(tf2.BufferCore, BufferInterface):
         :param source_frame: Name of the input frame.
         :param time: The time at which to get the transform (0 will get the latest).
         :param timeout: Time to wait for the target frame to become available.
-        :param return_debug_type: If true, return a tuple representing debug information.
+        :param return_debug_tuple: If true, return a tuple representing debug information.
         :return: The information of the transform being waited on.
         """
         clock = rclpy.clock.Clock()
@@ -272,7 +272,7 @@ class Buffer(tf2.BufferCore, BufferInterface):
         :param source_time: The time at which source_frame will be evaluated (0 gets the latest).
         :param fixed_frame: Name of the frame to consider constant in time.
         :param timeout: Time to wait for the target frame to become available.
-        :param return_debug_type: If true, return a tuple representing debug information.
+        :param return_debug_tuple: If true, return a tuple representing debug information.
         :return: The information of the transform being waited on.
         """
         clock = rclpy.clock.Clock()
