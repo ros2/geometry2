e805dafd41711852be2575f01f5aa55be096b2a3
Alejandro Hernández Cordero <ahcorde@gmail.com>
2026-03-23 10:07:38 +0100

    fixed typoe in buffer.py (#905)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    (cherry picked from commit 9a326e1b517d90f8f24db5f02ceb48f9dd030b24)
    
    # Conflicts:
    #	tf2_ros_py/tf2_ros/buffer.py

--- tf2_ros_py/tf2_ros/buffer.py
+++ tf2_ros_py/tf2_ros/buffer.py
@@ -211,8 +211,13 @@ class Buffer(tf2.BufferCore, tf2_ros.BufferInterface):
         :param source_frame: Name of the input frame.
         :param time: The time at which to get the transform (0 will get the latest).
         :param timeout: Time to wait for the target frame to become available.
+<<<<<<< HEAD
         :param return_debug_type: If true, return a tuple representing debug information.
         :return: True if the transform is possible, false otherwise.
+=======
+        :param return_debug_tuple: If true, return a tuple representing debug information.
+        :return: The information of the transform being waited on.
+>>>>>>> 9a326e1 (fixed typoe in buffer.py (#905))
         """
         clock = rclpy.clock.Clock()
         if timeout != Duration():
@@ -252,8 +257,13 @@ class Buffer(tf2.BufferCore, tf2_ros.BufferInterface):
         :param source_time: The time at which source_frame will be evaluated (0 will get the latest).
         :param fixed_frame: Name of the frame to consider constant in time.
         :param timeout: Time to wait for the target frame to become available.
+<<<<<<< HEAD
         :param return_debug_type: If true, return a tuple representing debug information.
         :return: True if the transform is possible, false otherwise.
+=======
+        :param return_debug_tuple: If true, return a tuple representing debug information.
+        :return: The information of the transform being waited on.
+>>>>>>> 9a326e1 (fixed typoe in buffer.py (#905))
         """
         clock = rclpy.clock.Clock()
         if timeout != Duration():
