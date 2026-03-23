c9c30c4538f9e490be6eb86e505a36e4a82071ab
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-03-23 12:46:25 +0100

    Fixed merge
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- tf2_ros_py/tf2_ros/buffer.py
+++ tf2_ros_py/tf2_ros/buffer.py
@@ -252,13 +252,8 @@ class Buffer(tf2.BufferCore, tf2_ros.BufferInterface):
         :param source_time: The time at which source_frame will be evaluated (0 will get the latest).
         :param fixed_frame: Name of the frame to consider constant in time.
         :param timeout: Time to wait for the target frame to become available.
-<<<<<<< HEAD
-        :param return_debug_type: If true, return a tuple representing debug information.
-        :return: True if the transform is possible, false otherwise.
-=======
         :param return_debug_tuple: If true, return a tuple representing debug information.
         :return: The information of the transform being waited on.
->>>>>>> 9a326e1 (fixed typoe in buffer.py (#905))
         """
         clock = rclpy.clock.Clock()
         if timeout != Duration():
