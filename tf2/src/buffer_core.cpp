7f89d0d2ab9d762acc6401c91b62d02c297b7a18
Alireza Moayyedi <137421365+alireza-moayyedi@users.noreply.github.com>
2026-09-07 19:40:29 +0200

    Fix ABBA deadlock between waitForTransform and testTransformableRequests (#982)
    (cherry picked from commit 16cfc1186b7e380f425589065dcb6cea5f587948)
    
    # Conflicts:
    #	tf2/src/buffer_core.cpp

--- tf2/src/buffer_core.cpp
+++ tf2/src/buffer_core.cpp
@@ -1405,34 +1405,40 @@ void BufferCore::_getFrameStrings(std::vector<std::string> & vec) const
 
 void BufferCore::testTransformableRequests()
 {
+<<<<<<< HEAD
   std::unique_lock<std::mutex> lock(transformable_requests_mutex_);
   V_TransformableRequest::iterator it = transformable_requests_.begin();
   while (it != transformable_requests_.end()) {
     TransformableRequest & req = *it;
+=======
+  struct PendingCallback
+  {
+    TransformableCallback cb;
+    TransformableRequestHandle request_handle;
+    std::string target_frame;
+    std::string source_frame;
+    TimePoint time;
+    TransformableResult result;
+  };
+  std::vector<PendingCallback> pending;
+>>>>>>> 16cfc11 (Fix ABBA deadlock between waitForTransform and testTransformableRequests (#982))
 
-    // One or both of the frames may not have existed when the request was originally made.
-    if (req.target_id == 0) {
-      req.target_id = lookupFrameNumber(req.target_string);
-    }
+  {
+    std::unique_lock<std::mutex> lock(transformable_requests_mutex_);
+    size_t i = 0;
+    while (i < transformable_requests_.size()) {
+      TransformableRequest & req = transformable_requests_[i];
 
-    if (req.source_id == 0) {
-      req.source_id = lookupFrameNumber(req.source_string);
-    }
+      // One or both of the frames may not have existed when the request was originally made.
+      if (req.target_id == 0) {
+        req.target_id = lookupFrameNumber(req.target_string);
+      }
 
-    TimePoint latest_time;
-    bool do_cb = false;
-    TransformableResult result = TransformAvailable;
-    // TODO(anyone): This is incorrect, but better than nothing. Really we want the latest time for
-    // any of the frames
-    getLatestCommonTime(req.target_id, req.source_id, latest_time, 0);
-    if ((latest_time != TimePointZero) && (req.time + cache_time_ < latest_time)) {
-      do_cb = true;
-      result = TransformFailure;
-    } else if (canTransformInternal(req.target_id, req.source_id, req.time, 0)) {
-      do_cb = true;
-      result = TransformAvailable;
-    }
+      if (req.source_id == 0) {
+        req.source_id = lookupFrameNumber(req.source_string);
+      }
 
+<<<<<<< HEAD
     if (do_cb) {
       {
         std::unique_lock<std::mutex> lock2(transformable_callbacks_mutex_);
@@ -1443,9 +1449,34 @@ void BufferCore::testTransformableRequests()
             req.request_handle, lookupFrameString(req.target_id), lookupFrameString(
               req.source_id), req.time, result);
           transformable_callbacks_.erase(req.cb_handle);
-        }
+=======
+      TimePoint latest_time;
+      bool do_cb = false;
+      TransformableResult result = TransformAvailable;
+      // TODO(anyone): This is incorrect, but better than nothing.
+      // Really we want the latest time for any of the frames.
+      getLatestCommonTime(req.target_id, req.source_id, latest_time, 0);
+      if ((latest_time != TimePointZero) && (req.time + cache_time_ < latest_time)) {
+        do_cb = true;
+        result = TransformFailure;
+      } else if (canTransformInternal(req.target_id, req.source_id, req.time, 0)) {
+        do_cb = true;
+        result = TransformAvailable;
       }
 
+      if (do_cb) {
+        TransformableCallback cb;
+        {
+          std::unique_lock<std::mutex> lock2(transformable_callbacks_mutex_);
+          auto cb_it = transformable_callbacks_.find(req.cb_handle);
+          if (cb_it != transformable_callbacks_.end()) {
+            cb = std::move(cb_it->second);
+            transformable_callbacks_.erase(cb_it);
+          }
+>>>>>>> 16cfc11 (Fix ABBA deadlock between waitForTransform and testTransformableRequests (#982))
+        }
+
+<<<<<<< HEAD
       if (transformable_requests_.size() > 1) {
         transformable_requests_[it -
           transformable_requests_.begin()] = transformable_requests_.back();
@@ -1459,7 +1490,30 @@ void BufferCore::testTransformableRequests()
       }
     } else {
       ++it;
+=======
+        if (cb) {
+          pending.push_back(
+            {std::move(cb), req.request_handle,
+              lookupFrameString(req.target_id), lookupFrameString(req.source_id),
+              req.time, result});
+        }
+
+        // Swap with the last element and pop to remove in O(1).
+        // Do not advance i: the element swapped in from the back is examined in the next iteration.
+        if (i < transformable_requests_.size() - 1) {
+          transformable_requests_[i] = transformable_requests_.back();
+        }
+        transformable_requests_.pop_back();
+      } else {
+        ++i;
+      }
+>>>>>>> 16cfc11 (Fix ABBA deadlock between waitForTransform and testTransformableRequests (#982))
     }
+  }
+
+  // Invoke callbacks without holding any mutex to prevent lock-order inversions.
+  for (auto & p : pending) {
+    p.cb(p.request_handle, p.target_frame, p.source_frame, p.time, p.result);
   }
 }
 
